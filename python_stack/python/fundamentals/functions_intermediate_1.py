4a44f93bdeada036d46288a185469286bae46edc
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-01 22:06:48 -0700

    edge cases for python functions intermediate 1

--- python_stack/python/fundamentals/functions_intermediate_1.py
+++ python_stack/python/fundamentals/functions_intermediate_1.py
@@ -5,6 +5,9 @@
 
 import random
 def randInt(min=0, max=100):
+    if min > max:
+        print('yikes')
+    
     num = random.random() * (max - min) + min
     return round(num)
 
@@ -12,3 +15,5 @@ print(randInt()) 		            # should print a random integer between 0 to 100
 print(randInt(max=50)) 	            # should print a random integer between 0 to 50
 print(randInt(min=50)) 	            # should print a random integer between 50 to 100
 print(randInt(min=50, max=500))     # should print a random integer between 50 and 500
+print(randInt(10, 1))               # should print a random integer between 1 and 10 
+print(randInt(10, -10))             # should print a random integer between -10 and 10
