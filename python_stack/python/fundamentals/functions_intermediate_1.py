a02928ca2e7358f0a41a76e2ebcb2f2e25ec2381
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-01 21:59:09 -0700

    finished python functons intermediate 1

---
+++ python_stack/python/fundamentals/functions_intermediate_1.py
@@ -0,0 +1,14 @@
+# If no arguments are provided, the function should return a random integer between 0 and 100.
+# If only a max number is provided, the function should return a random integer between 0 and the max number.
+# If only a min number is provided, the function should return a random integer between the min number and 100
+# If both a min and max number are provided, the function should return a random integer between those 2 values.
+
+import random
+def randInt(min=0, max=100):
+    num = random.random() * (max - min) + min
+    return round(num)
+
+print(randInt()) 		            # should print a random integer between 0 to 100
+print(randInt(max=50)) 	            # should print a random integer between 0 to 50
+print(randInt(min=50)) 	            # should print a random integer between 50 to 100
+print(randInt(min=50, max=500))     # should print a random integer between 50 and 500
