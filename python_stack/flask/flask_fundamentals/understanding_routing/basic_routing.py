31618cebdbc58265a6dcf55268ee1d0aa92e52c6
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-03 14:33:24 -0700

    finished ninja and sensei bonuses of basic routing

--- python_stack/flask/flask_fundamentals/understanding_routing/basic_routing.py
+++ python_stack/flask/flask_fundamentals/understanding_routing/basic_routing.py
@@ -16,7 +16,8 @@ def hello_dojo():
 # localhost:5000/say/flask - have it say "Hi Flask!"
 # localhost:5000/say/michael - have it say "Hi Michael!"
 # localhost:5000/say/john - have it say "Hi John!"
-@app.route('/say/<name>')
+# NINJA BONUS: Ensure the names provided in the 3rd task are strings
+@app.route('/say/<string:name>')
 def hi_name(name):
     return f'<h1>Hi {name.capitalize()}!</h1>'
 
@@ -24,13 +25,19 @@ def hi_name(name):
 # localhost:5000/repeat/35/hello - have it say "hello" 35 times
 # localhost:5000/repeat/80/bye - have it say "bye" 80 times
 # localhost:5000/repeat/99/dogs - have it say "dogs" 99 times
-@app.route('/repeat/<number>/<word>')
+# NINJA BONUS: For the 4th task, ensure the 2nd element in the URL is an integer, and the 3rd element is a string
+@app.route('/repeat/<int:number>/<string:word>')
 def repeat_word(number, word):
     resp = "<ol>"
     for i in range(int(number)):
         resp+= f"<li>{word}</li>"
     resp+="</ol>"
     return resp
+
+# SENSEI BONUS: Ensure that if the user types in any route other than the ones specified, they receive an error message saying "Sorry! No response. Try again."
+@app.route('/<path:path>')
+def catch_all(path):
+    return f"<h1>Sorry! No response from {path}. Try again.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
