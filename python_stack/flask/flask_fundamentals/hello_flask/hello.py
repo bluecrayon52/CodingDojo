44841f4f829d7cd74861d1b164d0640b15d6d8bc
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-04 15:32:30 -0700

    finished html table using Bootstrap

--- python_stack/flask/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -13,5 +13,16 @@ def hello_Testing():
 def hello_Name(name, num=1):
     return render_template('name.html', some_name= name, num_times= int(num))
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
