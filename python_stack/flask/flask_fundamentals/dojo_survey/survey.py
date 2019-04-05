3b190d41547373f795aa7e781481485af18adafa
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-04 22:35:08 -0700

    finished dojo survey

--- python_stack/flask/flask_fundamentals/dojo_survey/survey.py
+++ python_stack/flask/flask_fundamentals/dojo_survey/survey.py
@@ -6,21 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
             
-@app.route('/result/', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def create():
-    print("Got Post Info")
-    print(request.form)
     
     return render_template("show.html", 
         name = request.form['name'], 
-        location=request.form('location'), 
-        language=request.form('language'), 
-        comment=request.form('comment'))
+        location=request.form['location'], 
+        language=request.form['language'],
+        experience=request.form['experience'],
+        comment=request.form['comment'])
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-# http://localhost:5000 - have this display a nice looking HTML form.  The form should be submitted to '/result'
-# http://localhost:5000/result - have this display a html with the information that was submitted by POST
