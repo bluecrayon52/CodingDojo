155d7b9f1dc7320803b94ff2eaea5f50c6b3bdfb
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-09 09:44:59 -0700

    added form validation using flash messages

--- python_stack/flask/flask_mysql/first_flask_mysql/server.py
+++ python_stack/flask/flask_mysql/first_flask_mysql/server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
+app.secret_key = 'rfro4jr5g6oerv3on3roin'
+
 @app.route("/")
 def index():
     # call the function, passing in the name of our db
@@ -11,21 +13,36 @@ def index():
     print(friends)
     return render_template("index.html", friends=friends)
 
-@app.route("/create_friend", methods=["POST"])
+@app.route('/create_friend', methods=['POST'])
 def add_friend_to_db():
-    mysql = connectToMySQL('first_flask')
+    is_valid = True
+    if len(request.form['fname']) < 1:
+    	is_valid = False
+    	flash("Please enter a first name")
+    if len(request.form['lname']) < 1:
+    	is_valid = False
+    	flash("Please enter a last name")
+    if len(request.form['occ']) < 2:
+    	is_valid = False
+    	flash("Occupation should be at least 2 characters")
+    
+    if not is_valid:
+        return redirect("/")
+    else:
+    	# add user to database
+        mysql = connectToMySQL('first_flask')
+        query ="INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occup)s, Now(), Now());"
 
-    query ="INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occup)s, Now(), Now());"
+        data = {
+            'fn': request.form['fname'],
+            'ln': request.form['lname'],
+            'occup': request.form['occ']
+        }
 
-    data = {
-        'fn': request.form['fname'],
-        'ln': request.form['lname'],
-        'occup': request.form['occ']
-    }
+        new_friend_id = mysql.query_db(query, data)
+        flash("Friend successfully added!")
+        return redirect("/")    # eventually we may have a different success route
 
-    new_friend_id = mysql.query_db(query, data)
-
-    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
