2f19ef8357cc65d21743c4303df4b6a4d6d9e2f3
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-04-13 21:06:50 -0700

    passed location in url of farm and cave

--- python_stack/django/django_intro/test_project/apps/ninja_gold/urls.py
+++ python_stack/django/django_intro/test_project/apps/ninja_gold/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^process_money$', views.process),
-    url(r'^reset$', views.reset)
-
+    url(r'^reset$', views.reset),
+    url(r'^(?P<place>(farm|cave|house|casino))$', views.process_alt)
 ]
--- python_stack/django/django_intro/test_project/apps/ninja_gold/views.py
+++ python_stack/django/django_intro/test_project/apps/ninja_gold/views.py
@@ -48,6 +48,45 @@ def process(request):
                 'message': f'Lost {abs(amount)} golds from the casino! ({datetime.now()})',
                 'gain': False
                 })
+        return redirect('/ninja_gold')
+
+def process_alt(request, place):
+    if request.method == "GET":
+        if place == 'farm':
+            amount = random.randint(10, 20) 
+            request.session['gold'] += amount
+            request.session['messages'].insert(0, {
+                'message': f'Earned {amount} golds on the farm! ({datetime.now()})',
+                'gain': True
+            })
+            request.session['gain'] = True 
+        elif place == 'cave':
+            amount = random.randint(5, 10)
+            request.session['gold'] += amount
+            request.session['messages'].insert(0, {
+                'message': f'Found {amount} golds in the cave! ({datetime.now()})',
+                'gain': True
+            })
+        elif place == 'house':
+            amount = random.randint(2, 5) 
+            request.session['gold'] += amount
+            request.session['messages'].insert(0, {
+                'message': f'Earned {amount} golds from home! ({datetime.now()})',
+                'gain': True
+            })
+        else:
+            amount = random.randint(-50, 50)
+            request.session['gold'] += amount
+            if amount > 0:
+                request.session['messages'].insert(0, {
+                'message': f'Won {amount} golds from the casino! ({datetime.now()})',
+                'gain': True
+                })
+            else: 
+                request.session['messages'].insert(0, {
+                'message': f'Lost {abs(amount)} golds from the casino! ({datetime.now()})',
+                'gain': False
+                })
 
         return redirect('/ninja_gold')
 
